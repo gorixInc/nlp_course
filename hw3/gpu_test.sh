46ab25caed67ec25fab768e664a645e9347e760d
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-05-04 16:55:54 +0300

    asd

---
+++ hw3/gpu_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name="nlp_hw_gordei"
+#SBATCH --time 00:30:00 # set an appropriate amount of time for the job to run here in HH:MM:SS format
+#SBATCH --partition=gpu # set the partition to gpu
+#SBATCH --gres=gpu:tesla:1 # assign a single tesla gpu
+
+
+
+# Here you need to run train.py with python from the virtual environment where you have all the dependencies install
+# You also have to pass the command line args (such as dataset name) to the script here, as well
+# You may use whichever virtual environment manager you prefer (conda, venv, etc.)
+
+source activate nlp_course
+
+cd ~/nlp_course/hw3
+
+python test.py --dataset_name conll2003 --label_column_name tags --output_dir ./output --model_name_or_path distilbert-base-uncased
+
+conda deactivate
