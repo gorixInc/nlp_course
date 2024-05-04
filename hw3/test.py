80478821badd6a35ce1f15f0ef2afcee960ce87e
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-05-04 12:52:51 +0300

    asd

--- hw3/test.py
+++ hw3/test.py
@@ -1,3 +1,9 @@
 import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
+device = None
+try:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+except Exception as e:
+    device = e
+with open ('test_output.txt', mode='w+') as f:
+    f.write(device)
